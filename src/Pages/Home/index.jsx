c8433656da4e5b11f9503929f7165e9d7767c466
Andres Vargas <andrewgonzlez@gmail.com>
2025-02-24 17:55:07 -0500

    cambios look and feel plataforma inicio Home y registro

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,21 +4,21 @@ import RegisterForm from "../../Components/RegisterForm";
 function Home() {
   return (
     <Layout>
-      <div className="h-screen flex items-center justify-center">
+      <div className="static h-screen flex items-center justify-center">
         {/* Imagen de fondo */}
-        <div className="absolute inset-0">
+        <div className="absolute inset-0 pt-10">
           <img
             src="https://res.cloudinary.com/dehfpzadt/image/upload/v1740427688/mujer_profesional_hfoi30.jpg" // Reemplaza con la URL de tu imagen
             alt="Background"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover opacity-90"
           />
         </div>
       </div>
       {/* Contenedor del contenido */}
-      <div className="relative flex justify-between w-10/12">
+      <div className="absolute top-96 flex justify-between w-10/12">
         {/* Texto a la izquierda */}
-        <div className="text-white text-4xl font-bold w-1/2">
-          <h1 className="leading-tight">
+        <div className="absolute top-1 text-white text-4xl font-bold w-1/2">
+          <h1 className="leading-tight drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             Todo lo que necesitas para llegar a tus clientes
           </h1>
         </div>
